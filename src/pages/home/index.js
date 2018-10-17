4d75909ff253845356a0ae9398f35a9cecb6cfb1
luotianyi-wzd <ltywzd@gmail.com>
2018-10-18 01:25:06 +0800

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
 				<Card title='后台管理系统' style={{height:650}}>
 					<h1>技术栈</h1>
 					<p className='text'>react+antd+webpack+react-router-redex-echarts+ES6</p>
-					<p className='text'><a href="" target="_blank">github地址</a></p>
+					<p className='text'><a href="https://github.com/luotianyi-wzd/react---management" target="_blank">github地址</a></p>
 				</Card>
 
 			</div>
